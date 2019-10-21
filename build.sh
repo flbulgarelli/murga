333a2c3449a1d3ed2f21271021822864c13860c8
Franco Leonardo Bulgarelli <franco@mumuki.org>
2019-10-21 20:34:18 -0300

    Starting serves

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-
 set -e
+
+echo '   _____                              '
+echo '  /     \  __ _________  _________    '
+echo ' /  \ /  \|  |  \_  __ \/ ___\__  \   '
+echo '/    Y    \  |  /|  | \/ /_/  > __ \_ '
+echo '\____|__  /____/ |__|  \___  (____  / '
+echo '        \/            /_____/     \/  '
+
+#############
+## Clenaup ##
+#############
 
 rm -rf exercises
 rm -rf lessons
@@ -15,24 +25,71 @@ mkdir chapters
 mkdir -p character/kibi
 mkdir -p character/magnifying_glass
 
+
+###############
+## Functions ##
+###############
+
+function clone_and_start() {
+  repo=$1
+  install_script=$2
+  start_script=$3
+  port=$4
+  echo "[Murga] Checking whether there is a local copy of $repo..."
+  mkdir -p server
+  pushd server
+  if [[ ! -e $repo ]]; then
+    echo "[Murga] ... $repo not found! Clonning..."
+    git clone git@github.com:flbulgarelli/mumuki-$repo.git $repo
+    pushd mumuki-$repo
+    $install_script
+    popd
+  fi
+
+  echo "[Murga] Checking whether $repo is running..."
+  if ! lsof -i :$port; then
+    echo "[Murga] ... $repo is down. Staring it up..."
+    pushd $repo
+    $start_script >/dev/null 2>&1 &
+    popd
+  fi
+  popd
+}
+
+function on_exit() {
+  echo '[Murga] Killing danlingjobs'
+  for i in $(jobs -p); do
+    kill $i
+  done
+}
+
 function fetch() {
   source=$1
   destination=$2
   curl "http://localhost:3000/$source" -H 'Connection: keep-alive' \
-                                                    -H 'Pragma: no-cache' \
-                                                    -H 'Cache-Control: no-cache' \
-                                                    -H 'Upgrade-Insecure-Requests: 1' \
-                                                    -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36' \
-                                                    -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' \
-                                                    -H 'Accept-Encoding: gzip, deflate, br' \
-                                                    -H 'Accept-Language: en-US,en;q=0.9,es;q=0.8' \
-                                                    -H 'Cookie: login_organization=primaria; mucookie_session=N3pqbFhjbjlIWExmeFJMQ2xlK1N3NVhNQ0ZFZjdjemlBSHhNYVhySGk2az0tLWw1c1QwRUMxTnlIdVFmRjZ3ZEh3N0E9PQ%3D%3D--7a433a3f9284f53eba1c175ef8a5a4633c665222; mucookie_profile=eyJ1c2VyX25hbWUiOiIgIiwidXNlcl9pbWFnZV91cmwiOiJ1c2VyX3NoYXBl%0ALnBuZyJ9%0A; _mumuki_laboratory_session=MWhab3ZqTS9HOEUrY1h4aTZlTWM4UHU0VjlrMnlBcUVwRWtJdk01Nzk2R21MZFM1WW14M0txRGxkWGs2TnNsVmlBUGNoMThYblZ1K1N5WW4zS09WSytJUFRFNGJ1QXZJNGdIbUd0a0VvaFg1MTJWMWpjTG0veFdaZTQ5bEpySVlocFBiMWNZMXJkOE1QUFdIZ082MjZ3PT0tLWJJTEFYbkpROFZIK3dqYlg5TWFRbEE9PQ%3D%3D--6e6de6549a844c9e0b713629544fa102cace8ac2' \
-                                                    --compressed -s > $destination
+                                        -H 'Pragma: no-cache' \
+                                        -H 'Cache-Control: no-cache' \
+                                        -H 'Upgrade-Insecure-Requests: 1' \
+                                        -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36' \
+                                        -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' \
+                                        -H 'Accept-Encoding: gzip, deflate, br' \
+                                        -H 'Accept-Language: en-US,en;q=0.9,es;q=0.8' \
+                                        -H 'Cookie: login_organization=primaria; mucookie_session=N3pqbFhjbjlIWExmeFJMQ2xlK1N3NVhNQ0ZFZjdjemlBSHhNYVhySGk2az0tLWw1c1QwRUMxTnlIdVFmRjZ3ZEh3N0E9PQ%3D%3D--7a433a3f9284f53eba1c175ef8a5a4633c665222; mucookie_profile=eyJ1c2VyX25hbWUiOiIgIiwidXNlcl9pbWFnZV91cmwiOiJ1c2VyX3NoYXBl%0ALnBuZyJ9%0A; _mumuki_laboratory_session=MWhab3ZqTS9HOEUrY1h4aTZlTWM4UHU0VjlrMnlBcUVwRWtJdk01Nzk2R21MZFM1WW14M0txRGxkWGs2TnNsVmlBUGNoMThYblZ1K1N5WW4zS09WSytJUFRFNGJ1QXZJNGdIbUd0a0VvaFg1MTJWMWpjTG0veFdaZTQ5bEpySVlocFBiMWNZMXJkOE1QUFdIZ082MjZ3PT0tLWJJTEFYbkpROFZIK3dqYlg5TWFRbEE9PQ%3D%3D--6e6de6549a844c9e0b713629544fa102cace8ac2' \
+                                        --compressed -s > $destination
 
 }
 
-echo "[Murga] Fetching exercises..."
 
+############
+## Script ##
+############
+
+trap on_exit EXIT
+
+clone_and_start laboratory ./devinit ./devstart 3000
+clone_and_start "gobstones-runner" "bundle install" "bundle exec rackup" 9292
+
+echo "[Murga] Fetching exercises..."
 for i in {1715..1722}; do
   echo "[Murga] ...fetching exercise $i"
   fetch "primaria/exercises/$i" "exercises/$i.html"
