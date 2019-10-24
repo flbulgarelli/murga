5fcf775e7584918941b40cffe8e6aa2966a386fd
Franco Leonardo Bulgarelli <franco@mumuki.org>
2019-10-24 18:15:15 -0300

    Setting icon

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: 'logo-alt.png',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
