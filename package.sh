5fa8537a91fbdf8eb614f7433cce2dfa7a982ae3
Franco Leonardo Bulgarelli <franco@mumuki.org>
2019-10-28 13:11:39 -0300

    Adding more build platforms

--- package.sh
+++ package.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo "[Murga] Packaging for Linux..."
+electron-packager . --overwrite --platform=linux --arch=ia32
 electron-packager . --overwrite --platform=linux --arch=x64
 
 echo "[Murga] Packaging for Windows..."
 electron-packager . --overwrite --platform=win32 --arch=ia32
+electron-packager . --overwrite --platform=win32 --arch=x64
